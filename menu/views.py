89e82c5e7b6da82c319e9342880562ae88f85038
JinMay <britzegs@gmail.com>
2017-07-17 15:46:48 +0900

    test day_dict

--- menu/views.py
+++ menu/views.py
@@ -113,16 +113,27 @@ def answer(request):
     elif dorm_or_day in day:
         # if dorm_or_day == "월요일":
         #     menu = Main.objects.get(id = 1)
-        return JsonResponse({
-            "message": {
-                "text" : dorm_or_day + "식단 입니다." + str(menu_answer(dorm_or_day))
-            },
-            "keyboard": {
-                "type" : "buttons",
-                # 'buttons': keyboard_choice(dorm_or_day)
-                'buttons': ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일', '기숙사 선택']
+        if global_dorm == "본관":
+            day_dict = {
+                "월요일": 1,
+                "화요일": 2,
+                "수요일": 3,
+                "목요일": 4,
+                "금요일": 5,
+                "토요일": 6,
+                "일요일": 0,
             }
-        })
+            menu = Main.objects.get(day_dict[dorm_or_day])
+            return JsonResponse({
+                "message": {
+                    "text" : dorm_or_day + "식단 입니다.\n" + menu
+                },
+                "keyboard": {
+                    "type" : "buttons",
+                    # 'buttons': keyboard_choice(dorm_or_day)
+                    'buttons': ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일', '기숙사 선택']
+                }
+            })
     # 기숙사 선택을 눌렀을 때
     else:
         return JsonResponse({
