32fa4b00213f50cf63294b63b857077b385a2f42
JinMay <britzegs@gmail.com>
2017-09-04 11:46:10 +0900

    Edit admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -4,19 +4,23 @@ from .models import Main, Yangjin, Yangsung, Crj
 
 @admin.register(Main)
 class MainAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['number', 'day']
+    list_display_links = ['day']
 
 
 @admin.register(Yangsung)
 class YangsungAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['number', 'day']
+    list_display_links = ['day']
 
 
 @admin.register(Yangjin)
 class YangjinAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['number', 'day']
+    list_display_links = ['day']
 
 
 @admin.register(Crj)
 class CrjAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['number', 'day']
+    list_display_links = ['day']
